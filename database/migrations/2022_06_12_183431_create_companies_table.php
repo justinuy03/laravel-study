ee555650df5e4f63ffef8d23441375c7689fe70c
justinuy03 <justinuy303030@gmail.com>
2022-06-13 03:43:30 +0800

    Created columns for companies table

--- database/migrations/2022_06_12_183431_create_companies_table.php
+++ database/migrations/2022_06_12_183431_create_companies_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('website')->nullable();
+            $table->string('email');
             $table->timestamps();
         });
     }
