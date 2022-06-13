36912933c53a401bf2128384be9e52f0c75a7b14
justinuy03 <justinuy303030@gmail.com>
2022-06-13 08:15:00 +0800

    Created eloquent relationship for companies and contacts

---
+++ app/Models/Company.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Contact;
+
+class Company extends Model
+{
+    use HasFactory;
+    // protected $table = "my_table";
+    //protected $guarded = [];
+    protected $fillable = ['name', 'address', 'website', 'email'];
+
+    public function contacts(){
+        return $this->hasMany(Contact::class);
+    }
+}
---
+++ app/Models/Contact.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+    protected $fillable = ['first_name', 'Last_name', 'phone', 'email', 'address' ];
+
+    public function company(){
+        return $this->belongsTo(Company::class);
+    }
+}
