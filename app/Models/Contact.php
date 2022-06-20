6d3f7b7ba2f43f1bed352b7a4b257ac3c2040610
justinuy03 <justinuy303030@gmail.com>
2022-06-20 17:59:48 +0800

    Created local scope for contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -14,4 +14,19 @@ class Contact extends Model
     public function company(){
         return $this->belongsTo(Company::class);
     }
+
+    //local scope
+    public function scopeLatestFirst($query){
+        return $query->orderBy('id', 'desc');
+    }
+
+    public function scopeFilter($query){
+        if($companyId = request('company_id')){
+            $query->where('company_id', $companyId);
+        }
+        if($search = request('search')){
+            $query->where('first_name', 'LIKE', "%{$search}%");
+        }
+        return $query;
+    }
 }
